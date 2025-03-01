be0f4e14eb7bae01bb96f9c52fd15385b763026d
DellAce <agentmemov@gmail.com>
2025-03-01 21:37:45 +0300

    feat: сделали хуйню

---
+++ main.py
@@ -0,0 +1,14 @@
+from ucimlrepo import fetch_ucirepo
+
+# fetch dataset
+seoul_bike_sharing_demand = fetch_ucirepo(id=560)
+
+# data (as pandas dataframes)
+X = seoul_bike_sharing_demand.data.features
+y = seoul_bike_sharing_demand.data.targets
+
+# metadata
+print(seoul_bike_sharing_demand.metadata)
+
+# variable information
+print(seoul_bike_sharing_demand.variables)
